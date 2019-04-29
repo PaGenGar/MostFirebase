51d6cf79474763bbced01a19f4ec351998fddf94
most <most.most603@gmail.com>
2019-04-29 15:33:48 +0700

    Color Gradient font logo

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,19 +6,42 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget showAppName() {
+    return Text('Most Flutter',
+        style: TextStyle(
+            fontSize: 35.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.orange,
+            fontFamily: 'IndieFlower'));
+  }
 
-
-  Widget showAppName(){
-    return Text('Most Flutter');
+  Widget showLogo() {
+    return Image.asset('images/logo001.png');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.greenAccent, Colors.white],
+                begin: Alignment(0, -1))),
         padding: EdgeInsets.only(top: 100.0),
         alignment: Alignment(0, -1),
-        child: showAppName(),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 100.00,
+              height: 100.0,
+              child: showLogo(),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 15.0),
+              child: showAppName(),
+            ),
+          ],
+        ),
       ),
     );
   }
