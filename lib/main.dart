b0fbcab50aa42dc3d55aa156628fc5862222e7c3
most <most.most603@gmail.com>
2019-04-30 09:36:06 +0700

    Start 30 apr

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/authen.dart';
+import 'package:flutter/services.dart';
 void main() {
   runApp(MyApp());
 }
@@ -7,6 +8,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]);
     return MaterialApp(
       title: "Most Firebase",
       home: Authen(),
