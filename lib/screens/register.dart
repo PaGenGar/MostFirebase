aaa3539b19576780c7dad0d348ecb3fa4faf452a
most <most.most603@gmail.com>
2019-04-30 13:53:55 +0700

    Validator OK

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -5,86 +5,113 @@ class Register extends StatefulWidget {
   _RegisterState createState() => _RegisterState();
 }
 
-Widget passTeatFormField() {
-  return TextFormField(
-    obscureText: true,
-    decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(width: 1.0, color: Colors.grey),
-            borderRadius: BorderRadius.circular(10.0)),
-        labelText: 'Password',
-        hintText: 'You Password',
-        icon: Icon(
-          Icons.lock,
-          color: Colors.orange,
-        )),
-  );
-}
-
-Widget nameTextFormField() {
-  return TextFormField(
-    decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10.0),
-            borderSide: BorderSide(width: 1.0, color: Colors.grey)),
-        labelText: 'Name',
-        hintText: 'Type Your Name',
-        icon: Icon(
-          Icons.face,
-          color: Colors.red,
-        )),
-  );
-}
-
-Widget emailTextFormField() {
-  return TextFormField(
-    decoration: InputDecoration(
-        enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(width: 1.0, color: Colors.grey),
-            borderRadius: BorderRadius.circular(10.0)),
-        labelText: 'Email',
-        hintText: 'you@email.com',
-        icon: Icon(
-          Icons.email,
-          color: Colors.blue,
-        )),
-  );
-}
-
 class _RegisterState extends State<Register> {
+  //Explicit
+  final formKey = GlobalKey<FormState>();
+
+  Widget passTeatFormField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1.0, color: Colors.grey),
+              borderRadius: BorderRadius.circular(10.0)),
+          labelText: 'Password',
+          hintText: 'You Password',
+          icon: Icon(
+            Icons.lock,
+            color: Colors.orange,
+          )),
+      validator: (String value) {
+        if (value.length<=5) {
+          return 'Please Type Password more Charactor';
+        }
+      },
+    );
+  }
+
+  Widget nameTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide(width: 1.0, color: Colors.grey)),
+          labelText: 'Name',
+          hintText: 'Type Your Name',
+          icon: Icon(
+            Icons.face,
+            color: Colors.red,
+          )),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Plase Full Name in the blank';
+        }
+      },
+    );
+  }
+
+  Widget emailTextFormField() {
+    return TextFormField(
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(width: 1.0, color: Colors.grey),
+              borderRadius: BorderRadius.circular(10.0)),
+          labelText: 'Email',
+          hintText: 'you@email.com',
+          icon: Icon(
+            Icons.email,
+            color: Colors.blue,
+          )),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'กรุณากรอก email';
+        } else if (!((value.contains('@')) && (value.contains('.')))) {
+          return 'กรุณากรอก Email ให้ตรง Format';
+        }
+      },
+    );
+  }
 
   Widget uploadButton() {
     return IconButton(
       icon: Icon(Icons.cloud_upload),
-      tooltip: 'Upload To Firebase',onPressed: (){print('you click upload');},
+      tooltip: 'Upload To Firebase',
+      onPressed: () {
+        print('you click upload');
+        if (formKey.currentState.validate()) {}
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.redAccent,
-        title: Text('Register'),
-        actions: <Widget>[uploadButton()],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: RadialGradient(
-                colors: [Colors.white, Colors.blue],
-                radius: 2.5,
-                center: Alignment(-1, -1))),
-        padding: EdgeInsets.all(50.0),
-        child: Column(
-          children: <Widget>[
-            nameTextFormField(),
-            Container(
-              margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
-              child: emailTextFormField(),
-            ),
-            passTeatFormField()
-          ],
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text('Register'),
+          actions: <Widget>[uploadButton()],
         ),
-      ),
-    );
+        body: Form(
+          key: formKey,
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: RadialGradient(
+                    colors: [Colors.white, Colors.blue],
+                    radius: 2.5,
+                    center: Alignment(-1, -1))),
+            padding: EdgeInsets.all(50.0),
+            child: Column(
+              children: <Widget>[
+                nameTextFormField(),
+                Container(
+                  margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: emailTextFormField(),
+                ),
+                passTeatFormField()
+              ],
+            ),
+          ),
+        ));
   }
 }
